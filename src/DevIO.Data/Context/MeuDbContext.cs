2c64055aab856791300497858307b12125db2dda
Jean <jeanmendesf@gmail.com>
2020-12-31 17:40:29 -0300

    Add project files.

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,33 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options): base(options){}
+
+        public DbSet<Produto> Produtos { get;  set; }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos{ get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Configuração pro caso de esquecer de configurar alguma propriedade, a propriedade será salva na configuração abaixo
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                .Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(100)";
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //Desativar CascadeDelete
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
