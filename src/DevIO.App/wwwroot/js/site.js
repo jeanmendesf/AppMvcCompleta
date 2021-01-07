8ac21f6b156f3c979cb1f357c8286144e35657f1
Jean <jeanmendesf@gmail.com>
2021-01-07 17:19:22 -0300

    Implementando um buscador de CEP

--- src/DevIO.App/wwwroot/js/site.js
+++ src/DevIO.App/wwwroot/js/site.js
@@ -39,3 +39,62 @@
         }
     });
 }
+
+
+function BuscaCep() {
+    $(document).ready(function () {
+
+        function limpa_formulario_cep() {
+            $("#Endereco_Logradouro").val("");
+            $("#Endereco_Bairro").val("");
+            $("#Endereco_Cidade").val("");
+            $("#Endereco_Estado").val("");
+        }
+
+        $("#Endereco_Cep").blur(function () {
+
+            //variável "cep" somente com digitos
+            var cep = $(this).val().replace(/\D/g, '');
+
+            //Verifica se possui algum valor
+            if (cep != "") {
+
+                //Expressão para validar o CEP.
+                var validacep = /^[0-9]{8}$/;
+
+                //Valida o formato do CEP
+                if (validacep.test(cep)) {
+                    //Preenche os campos com "..." enquanto consulta o webService.
+                    $("#Endereco_Logradouro").val("...");
+                    $("#Endereco_Bairro").val("...");
+                    $("#Endereco_Cidade").val("...");
+                    $("#Endereco_Estado").val("...");
+
+                    //Consulta o webservice viacep.com.br/
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+                        function (dados) {
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+                                $("#Endereco_Logradouro").val(dados.logradouro);
+                                $("#Endereco_Bairro").val(dados.bairro);
+                                $("#Endereco_Cidade").val(dados.localidade);
+                                $("#Endereco_Estado").val(dados.uf);
+                            } else {
+                                limpa_formulario_cep();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                }
+                else {
+                    //Quando o cep for inválido
+                    limpa_formulario_cep();
+                    alert("Formato de CEP inválido.");
+                }
+            }
+            else {
+                //Quando o Cep for sem valor
+                limpa_formulario_cep();
+            }
+        });
+    });
+}
