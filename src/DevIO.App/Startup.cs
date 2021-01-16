d77fe3ef9b96c62b79a5a6cb98237e3b4fd368cf
Jean <jeanmendesf@gmail.com>
2021-01-15 23:13:13 -0300

    Tratando erros

--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -56,7 +56,7 @@ namespace DevIO.App
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/erro/500");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseStatusCodePagesWithRedirects("/erro{0}");
                 app.UseHsts();
